8dbd2146595030c5864e408b7c7e5817d3b155b3
srvz <docdedi@gmail.com>
2015-12-08 10:50:56 +0800

    dist

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.notify = exports.rsa = exports.md5 = exports.utils = undefined;
 
 require('babel-polyfill');
 
@@ -10,6 +11,26 @@ var _alipay = require('./alipay');
 
 var _alipay2 = _interopRequireDefault(_alipay);
 
+var _utils = require('./utils');
+
+var _utils2 = _interopRequireDefault(_utils);
+
+var _md = require('./md5');
+
+var _md2 = _interopRequireDefault(_md);
+
+var _rsa = require('./rsa');
+
+var _rsa2 = _interopRequireDefault(_rsa);
+
+var _notify = require('./notify');
+
+var _notify2 = _interopRequireDefault(_notify);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+exports.utils = _utils2.default;
+exports.md5 = _md2.default;
+exports.rsa = _rsa2.default;
+exports.notify = _notify2.default;
 exports.default = _alipay2.default;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import 'babel-polyfill';
 import Alipay from './alipay';
+import utils from './utils';
+import md5 from './md5';
+import rsa from './rsa';
+import notify from './notify';
 
+export {utils, md5, rsa, notify};
 export default Alipay;
